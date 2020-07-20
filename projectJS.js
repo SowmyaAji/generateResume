5121bc922ba17d1e8b0ea2c85371758a509efb62
Sowmya Aji <sowmya.aji@gmail.com>
2020-07-20 00:34:50 -0400

    Add remove and add education functionality

--- projectJS.js
+++ projectJS.js
@@ -21,7 +21,7 @@ function generateHeader() {
 function addEmpt() {
   let empt = document.createElement("div");
   empt.setAttribute("id", "empt");
-  let para = document.createElement("div");
+  let para = document.createElement("p");
   let label = document.createElement("label");
   label.htmlFor = "startDate";
   let labelText = document.createTextNode(
@@ -73,6 +73,84 @@ function addEmpt() {
 function removeEmpt() {
   const empt = document.getElementById("empt");
   document.getElementById("employmentDeets").removeChild(empt);
+}
+
+// add education
+function addEdu() {
+  let edu = document.createElement("div");
+  edu.setAttribute("id", "edu");
+  let para = document.createElement("p");
+  let label = document.createElement("label");
+  label.htmlFor = "degree";
+  let labelText = document.createTextNode(
+    "Enter degree, certificate or course name: "
+  );
+  label.appendChild(labelText);
+  let linebreak = document.createElement("br");
+  label.appendChild(linebreak);
+  let input = document.createElement("input");
+  input.setAttribute("type", "text");
+  input.setAttribute("name", "degree");
+  input.setAttribute("class", "degree");
+  label.appendChild(input);
+  para.appendChild(label);
+  edu.appendChild(para);
+
+  para = document.createElement("p");
+  label = document.createElement("label");
+  label.htmlFor = "major";
+  labelText = document.createTextNode("Enter major(s): ");
+  label.appendChild(labelText);
+  linebreak = document.createElement("br");
+  label.appendChild(linebreak);
+  input = document.createElement("input");
+  input.setAttribute("type", "text");
+  input.setAttribute("name", "major");
+  input.setAttribute("class", "major");
+  label.appendChild(input);
+  para.appendChild(label);
+  edu.appendChild(para);
+
+  para = document.createElement("p");
+  label = document.createElement("label");
+  label.htmlFor = "gpa";
+  labelText = document.createTextNode(
+    "Enter GPA/Grade or score out of total:  "
+  );
+  label.appendChild(labelText);
+  linebreak = document.createElement("br");
+  label.appendChild(linebreak);
+  input = document.createElement("input");
+  input.setAttribute("type", "text");
+  input.setAttribute("name", "gpa");
+  input.setAttribute("class", "gpa");
+  label.appendChild(input);
+  para.appendChild(label);
+  edu.appendChild(para);
+
+  para = document.createElement("p");
+  label = document.createElement("label");
+  label.htmlFor = "year";
+  labelText = document.createTextNode(
+    "Enter date or expected date of completion: "
+  );
+  label.appendChild(labelText);
+  linebreak = document.createElement("br");
+  label.appendChild(linebreak);
+  input = document.createElement("input");
+  input.setAttribute("type", "date");
+  input.setAttribute("name", "year");
+  input.setAttribute("class", "year");
+  label.appendChild(input);
+  para.appendChild(label);
+  edu.appendChild(para);
+  document.getElementById("educationDeets").appendChild(edu);
+}
+
+// remove education
+function removeEdu() {
+  const edu = document.getElementById("edu");
+  document.getElementById("educationDeets").removeChild(edu);
 }
 
 function validateInput(email) {
@@ -140,6 +218,20 @@ function getMonth(date) {
   }
 }
 
+function getEducation(degree, major, gpa, year) {
+  let myText =
+    "<div><div class='left'>" +
+    degree +
+    "</div><div class='right'>" +
+    major +
+    "<br>GPAGrade: " +
+    gpa +
+    "<br>Year completed or expected: " +
+    year +
+    "</div></div><br>\n";
+  return myText;
+}
+
 // set the employment years and details in columns for each entry
 function getEmployment(startDate, endDate, employment) {
   let endYear = getYear(endDate);
@@ -162,7 +254,6 @@ function getEmployment(startDate, endDate, employment) {
   return myText;
 }
 
-
 // get all the inputs and put them into formatted html
 function generateHTML() {
   const fullName = document.getElementById("fullName").value;
@@ -173,13 +264,14 @@ function generateHTML() {
   const validatedEmail = validateEmail(email);
   const career = document.getElementById("careerObj").value;
   const personal = document.getElementById("peronalInfo").value;
-  const education = document.getElementById("education").value;
-  const employments = document.getElementById("employmentDeets").childNodes;
+  const education = document.getElementById("educationDeets").childNodes;
   // convert the child nodelist into an array
+  const eduArray = [...education];
+  const employments = document.getElementById("employmentDeets").childNodes;
+
   const empArray = [...employments];
   const references = document.getElementById("references").value;
   const bizReferences = document.getElementById("bizReferences").value;
-  
 
   // return html only if the email input is valid
   if (validateInput(email)) {
@@ -199,9 +291,19 @@ function generateHTML() {
       personal +
       "</div></div><br>";
     myText +=
-      "<div><div class='left'> EDUCATION </div><div class='right'>" +
-      education +
-      "</div></div><br>";
+      "<div><div class='left'> EDUCATION </div></div>";
+      //  class='right'>" +
+      // education +
+      // "</div></div><br>";
+    if (eduArray.length > 0) {
+      for (let i = 1; i < eduArray.length; i++) {
+        let degree = eduArray[i].querySelector(".degree").value;
+        let major = eduArray[i].querySelector(".major").value;
+        let gpa = eduArray[i].querySelector(".gpa").value;
+        let year = eduArray[i].querySelector(".year").value;
+        myText += getEducation(degree, major, gpa, year);
+      }
+    }
     myText += "<div><div class='left'> EMPLOYMENT DETAILS </div></div>";
     if (empArray.length > 0) {
       for (let i = 1; i < empArray.length; i++) {
@@ -223,8 +325,9 @@ function generateHTML() {
       "<div><div class='left'> BUSINESS REFERENCES </div><div class='right'>" +
       bizReferences +
       "</div></div>\n";
-     // give print option from browser 
-    myText += "<div class='right'><br><input type='button' id='download' name='download' value='Download' onclick='window.print()' /></div>"
+    // give print option from browser
+    myText +=
+      "<div class='right'><br><input type='button' id='download' name='download' value='Download' onclick='window.print()' /></div>";
     myText += "</body>\n</html>";
     return myText;
   }
@@ -241,9 +344,8 @@ function createResume() {
       "width=800,height=800,left=200,top=200"
     );
     flyWindow.document.write(myText);
-     }
+  }
 }
-
 
 function addListener(obj, type, fn) {
   // if browser supports event listener
@@ -256,7 +358,9 @@ function addListener(obj, type, fn) {
   }
 }
 
+
+addListener(document.getElementById("addEdu"), "click", addEdu);
+addListener(document.getElementById("removeEdu"), "click", removeEdu);
 addListener(document.getElementById("add"), "click", addEmpt);
 addListener(document.getElementById("remove"), "click", removeEmpt);
 addListener(document.getElementById("create"), "click", createResume);
-
